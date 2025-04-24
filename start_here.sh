ee1b80de4385a6ab933b8acc6c0a3acd4eb527f9
Rich Moulton <rich.moulton@trojans.dsu.edu>
2025-04-24 10:49:02 -0400

    Add new logs

--- start_here.sh
+++ start_here.sh
@@ -1,3 +1,4 @@
+python build_vector_index.py
 uvicorn api:app --port 8000 &
-sleep 10
-garak --model_type rest -G ./garak_config.json &
+sleep 100
+garak -m rest -p rag_pii_probe -p latentinjection -p atkgen -p promptinject -p divergence -G ./garak_config.json --verbose
