8d9c7a8809089b6be29f4767d30262c38fb9f5c8
Rich Moulton <rich.moulton@trojans.dsu.edu>
2025-04-24 09:52:11 -0400

    Run only selected prompts

--- start_here.sh
+++ start_here.sh
@@ -1,4 +1,4 @@
 python build_vector_index.py
 uvicorn api:app --port 8000 &
 sleep 100
-garak --model_type rest -G ./garak_config.json &
+garak -m rest -p rag_pii_probe -p latentinjection -p atkgen -p promptinject -p divergence -G ./garak_config.json --verbose
