8d03b5e0113b5552c426d2ff1c158b0b1673c0d8
Rich Moulton <rich.moulton@trojans.dsu.edu>
2025-04-25 19:17:34 -0400

    Remove extraneous comment

--- api.py
+++ api.py
@@ -29,7 +29,7 @@ app = FastAPI()
 # Request model
 class TextRequest(BaseModel):
     prompt: str
-    enable_rag: bool = True  # RAG is disabled by default
+    enable_rag: bool = True  # RAG is enabled by default
 
 
 # Load generation model
